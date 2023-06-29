e52e1016ea1932e4c6e164852b273cb9ed86b0a1
=bytecodeinertia <=caesardave744@gmail.com>
2023-06-29 17:35:54 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function goBack(){
+    window.history.back()
+};
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  };
+};
